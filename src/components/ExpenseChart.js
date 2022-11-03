e6cebd083cb8cac41a577c4cf21fcf033d196618
james bariuad <jamesbariuad@gmail.com>
2022-11-03 23:33:30 +0800

    test

--- src/components/ExpenseChart.js
+++ src/components/ExpenseChart.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Doughnut } from "react-chartjs-2";
-// import {Chart as chartjs} from "chart.js/auto"
+import {Chart as chartjs} from "chart.js/auto"
 
 const ExpenseChart = ({ expense }) => {
   return <Doughnut data={expense}/>;
